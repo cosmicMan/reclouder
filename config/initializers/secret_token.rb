57db2ce53e0fe83ff3229bccd339eeecb434367d
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2012-09-29 09:34:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reclouder::Application.config.secret_token = 'b08f5595a241e3a6fd6155221e020a0f39843721a902b6c04d5e9f2d8dcbe0f1d8d9efcd1b70ed0d6e39499b54224d40f7fd86a96f0fe2d4603e73ae42ec82e7'
